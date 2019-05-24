65a7a77478695756db7b59746accc5eb9b6b7e8a
abel555 <taqui555@gmail.com>
2019-05-24 16:36:31 -0400

    funciones sin usar borradas

--- Assets/Scripts/DestroyEnemies.cs
+++ Assets/Scripts/DestroyEnemies.cs
@@ -3,16 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyEnemies : MonoBehaviour
-{
-    void Start()
-    {
-        
-    }
-    void Update()
-    {
-        
-    }
-    
+{ 
     void OnCollisionEnter(Collision c) {
         if (c.gameObject.name == "Capsule")
             Destroy(c.gameObject);
