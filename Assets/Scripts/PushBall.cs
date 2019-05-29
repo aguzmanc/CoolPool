26d1fcaa9de120177b7e3776b1eeaddb7abc9239
abel555 <taqui555@gmail.com>
2019-05-29 18:09:11 -0400

    detecta el click del mouse

---
+++ Assets/Scripts/PushBall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushBall : MonoBehaviour
+{
+    public float speed = 5;
+    Rigidbody ballRigidbody;
+   
+    void Start()
+    {
+        ballRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0) &&
+        Physics.Raycast(ray, out hit)) {
+            Vector3 direction = hit.point;
+            direction.y = 0.0f;
+            ballRigidbody.AddForce(direction * speed, ForceMode.Impulse);
+        }
+    }
+}
