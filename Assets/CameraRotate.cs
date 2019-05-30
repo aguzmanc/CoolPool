26946b8aa0d8ee87b9d41d97ff378dce4064c7e5
Carlos Jorge Cary Huanca <gary7412@hotmail.com>
2019-05-30 17:43:06 -0400

    Camara corrections

--- Assets/CameraRotate.cs
+++ Assets/CameraRotate.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class CameraRotate : MonoBehaviour
 {   
     public float speed;
-    public float prueba;
     
     void Start() {
         
     }
+
     void Update() {
-        prueba = Input.mouseScrollDelta.y;
-        transform.Rotate(0, 0, Input.mouseScrollDelta.y * speed);
+        transform.Rotate(0, Input.mouseScrollDelta.y * speed, 0, 0);
     }
 }
