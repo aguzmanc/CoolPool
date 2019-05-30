c21cd59cd4b553b9b5218f207f18b26e3c81dc09
Carlos Jorge Cary Huanca <gary7412@hotmail.com>
2019-05-30 17:17:10 -0400

    Move camera with scroll up and scroll down

---
+++ Assets/CameraRotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{   
+    public float speed;
+    public float prueba;
+    
+    void Start() {
+        
+    }
+    void Update() {
+        prueba = Input.mouseScrollDelta.y;
+        transform.Rotate(0, 0, Input.mouseScrollDelta.y * speed);
+    }
+}
