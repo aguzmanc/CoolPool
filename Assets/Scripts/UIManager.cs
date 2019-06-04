bb33a1f6b4973ab97e61a67c87bae8f485ee200f
Carlos Jorge Cary Huanca <gary7412@hotmail.com>
2019-06-04 15:24:38 -0400

    uimanager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    enum GameEndings {GameOver, Victory};
+    Transform buttons;
+
+    void Start() {
+        buttons = transform.GetChild(0);
+        print(buttons.name);
+    }
+
+    void Update() {
+    }
+
+    public void ShowGameEndOverLay() {
+        
+    }
+}
