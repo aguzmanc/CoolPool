c516a5ffa0f9ea30d0a0b6260df8be4c599eb505
arn <spacerockganimedes@gmail.com>
2019-06-04 15:07:21 -0400

    Clase de Singleton. Trabajar sobre esto

--- Assets/Scripts/ChangeLevel.cs
+++ Assets/Scripts/ChangeLevel.cs
@@ -6,12 +6,54 @@ using UnityEngine.SceneManagement;
 
 public class ChangeLevel : MonoBehaviour
 {
+    private static ChangeLevel _instance;
+
+    public static ChangeLevel instance{
+        get{ 
+            if(_instance==null){
+                GameObject go = new GameObject();
+                _instance = go.AddComponent<ChangeLevel>();
+            }
+
+
+            return _instance;
+        }
+        //set{}
+    }
+
+    private int _algo;
+    public int algo{
+        get{ return _algo;}
+        set{ _algo = value;}
+    }
+    public int GetAlgo(){return _algo;}
+    public void SetAlgo(int algo){
+        _algo = algo;
+    }
+
+    public static ChangeLevel GetInstance(){
+        return _instance;
+    }
+    //public static ChangeLevel instance;
+
+
     public Object[] scenes;
     static int currentLevel;
     List<string> levels = new List<string>();
 
 
     void Start(){
+        if(_instance==null)
+            Debug.Log("no hay otro");
+        else {
+            Debug.Log("Si hay otro, ERROR!!");
+            Destroy(gameObject);
+            return;
+        }
+        
+
+        _instance = this;
+
         for (int i = 0; i < scenes.Length ; i++){
             SceneAsset level = scenes[i] as SceneAsset;
             levels.Add(level.name);
---
+++ Assets/Scripts/UINivel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UINivel : MonoBehaviour
+{
+   public void SiguienteNivel(){
+        //ChangeLevel level = GameObject.FindObjectOfType<ChangeLevel>();
+
+        //ChangeLevel.GetInstance().NextScene();
+        ChangeLevel.instance.NextScene();
+        //ChangeLevel.instance.NextScene();
+
+        //ChangeLevel.NextScene();
+        //level.NextScene();
+
+        //ChangeLevel._instance = null; // no es posible acceder
+    }
+
+    public void AnteriorNivel(){}
+
+    public void JugarDeNuevo(){}
+}
