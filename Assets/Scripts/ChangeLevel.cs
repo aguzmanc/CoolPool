c6667d9e7c13ac660875c07f0ae3a768a800452c
abel555 <taqui555@gmail.com>
2019-06-03 20:23:46 -0400

    bug que no dejaba volver atras en nivel 3 corregido

--- Assets/Scripts/ChangeLevel.cs
+++ Assets/Scripts/ChangeLevel.cs
@@ -7,32 +7,36 @@ using UnityEngine.SceneManagement;
 public class ChangeLevel : MonoBehaviour
 {
     public Object[] scenes;
-    static int currentLevel = 0;
-    List<SceneAsset> levels = new List<SceneAsset>();
-    void Start()
-    {
+    static int currentLevel;
+    List<string> levels = new List<string>();
+
+
+    void Start(){
         for (int i = 0; i < scenes.Length ; i++){
-             levels.Add( scenes[i] as SceneAsset);
-          
+            SceneAsset level = scenes[i] as SceneAsset;
+            levels.Add(level.name);
         }
+        currentLevel = levels.IndexOf(SceneManager.GetActiveScene().name);
+        print(currentLevel); 
+        print("niveles :" + levels.Count);
     }
+
+
     public void NextScene() {
         if (currentLevel < levels.Count - 1 ) {
-            SceneManager.LoadScene(levels[currentLevel + 1].name);
-            currentLevel ++;
+            SceneManager.LoadScene(levels[currentLevel + 1]);
         }
     }
 
 
     public void PreviousScene() {
         if (currentLevel > 0) {
-            SceneManager.LoadScene(levels[currentLevel - 1].name);
-            currentLevel --;
+            SceneManager.LoadScene(levels[currentLevel - 1]);
         }
     }
 
     public void ReloadScene() {
-        SceneManager.LoadScene(levels[currentLevel].name);
+        SceneManager.LoadScene(levels[currentLevel]);
     }
     
 }
