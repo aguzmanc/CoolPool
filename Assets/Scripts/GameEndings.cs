0d883a3e0ca17ea698ac36c1c5b4ee9ee9e813fd
Carlos Jorge Cary Huanca <gary7412@hotmail.com>
2019-06-06 17:22:26 -0400

    UIgamaner working with layouts win and lose with animations

--- Assets/Scripts/GameEndings.cs
+++ Assets/Scripts/GameEndings.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RepeatLevelButton : MonoBehaviour
+public enum GameEndings
 {
-    
+    Victory = 0,
+    GameOver = 1
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,18 +4,57 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    enum GameEndings {GameOver, Victory};
-    Transform buttons;
+    private static UIManager _instance;
+    Animator victoryAnimation;
+    Animator gameOverAnimation;
 
+    Animator backLevelButton;
+    Animator repeatLevelButton;
+    Animator nextLevelButton;
+
+    public static UIManager instance{
+        get{
+            return _instance;
+        }
+    }
+    
     void Start() {
-        buttons = transform.GetChild(0);
-        print(buttons.name);
+        _instance = this;
+        victoryAnimation = GameObject.Find("Victory").GetComponent<Animator>();
+        gameOverAnimation = GameObject.Find("GameOver").GetComponent<Animator>();
+
+        backLevelButton = GameObject.Find("BackLevel").GetComponent<Animator>();
+        repeatLevelButton = GameObject.Find("RepeatLevel").GetComponent<Animator>();
+        nextLevelButton = GameObject.Find("NextLevel").GetComponent<Animator>();
     }
 
     void Update() {
+        
+    }
+    public void ShowVictoryOverLay() {
+        victoryAnimation.SetBool("Activate", true);
+        LevelChangeButtons();
+    }
+    
+    public void ShowGameEndOverLay() {
+        gameOverAnimation.SetBool("Activate", true);
+        LevelChangeButtons();
     }
 
-    public void ShowGameEndOverLay() {
-        
+    public void LevelChangeButtons() {
+        backLevelButton.SetBool("Activate", true);
+        repeatLevelButton.SetBool("Activate", true);
+        nextLevelButton.SetBool("Activate", true);
+    }
+
+    public void ShowGameEndedOverlay(GameEndings ending) {
+        switch(ending) {
+            case GameEndings.Victory:
+                ShowVictoryOverLay();
+                break;
+            case GameEndings.GameOver:
+                ShowGameEndOverLay();
+                break;
+        }
     }
 }
