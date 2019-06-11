9c2bac05da26a0a23d04a2f4e1f619173f801e3f
abel555 <taqui555@gmail.com>
2019-06-11 08:58:48 -0400

    cooldown agregado a la bola

--- Assets/Scripts/PushBall.cs
+++ Assets/Scripts/PushBall.cs
@@ -5,27 +5,39 @@ using UnityEngine;
 public class PushBall : MonoBehaviour
 {
     public float strength = 30;
+    public float cooldown = 5;
+    float cd;
     Rigidbody ballRigidbody;
     void Awake()
     {
         ballRigidbody = GetComponent<Rigidbody>();
     }
 
+    void Update() {
+        CooldownTimer();
+    }
 
     void OnCollisionEnter(Collision other)
     {   Transform c = other.gameObject.transform;
-        if (c.GetComponent<FollowTarget>()) {
+        if (c.GetComponent<FollowTarget>() && cd <= 0) {
             Vector3 direction = transform.position - c.parent.transform.position;
             direction.y = 0;
             Vector3 position = other.GetContact(0).point;
             position.y = 0;
             ballRigidbody.AddForceAtPosition(direction.normalized * strength, position, ForceMode.Impulse);
+            cd = cooldown;
         }
     }
+
     void OnTriggerEnter(Collider other)
     {
         if(other.transform.GetComponent<Goal>()) {
            GameManager.TriggerVictory();
         }
+    }
+    
+    void CooldownTimer() {
+        if (cd > 0)
+            cd = cd - (1 * Time.deltaTime);
     }
 }
