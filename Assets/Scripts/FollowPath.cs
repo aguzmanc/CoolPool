c9a9f652c41acf4bb8ffb1e78561a2d21f56536c
abel555 <taqui555@gmail.com>
2019-06-11 10:00:11 -0400

    los enemigos se mueven en direccion opuesta cuando la velocidad es negativa

--- Assets/Scripts/FollowPath.cs
+++ Assets/Scripts/FollowPath.cs
@@ -12,14 +12,18 @@ public class FollowPath : MonoBehaviour
 
     void Start()
     {
-        nChilds = path.childCount -1;
+        nChilds = path.childCount - 1;
+        if (speed < 0){
+            child_number = nChilds;
+        }
         findTarget();
+        
     }
 
 
     void Update()
     {
-        float deltaDistance = Time.deltaTime * speed;
+        float deltaDistance = Time.deltaTime * Mathf.Abs(speed);
         transform.position =
             Vector3.MoveTowards(transform.position,
                                 target,
@@ -32,11 +36,22 @@ public class FollowPath : MonoBehaviour
     
     void findTarget(){
         target = path.GetChild(child_number).transform.position;
-        if(child_number >= nChilds){
+        if (speed > 0){
+            if(child_number >= nChilds){
                 child_number = 0;
+            }
+            else{
+                child_number++;
+            }
         }
-        else{
-            child_number++;
+        if (speed < 0){
+            if(child_number <= 0){
+                child_number = nChilds;
+            }
+            else{
+                child_number--;
+            }
         }
+        
     }
 }
