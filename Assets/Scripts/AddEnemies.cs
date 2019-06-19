04edc0a7a0fc4f17d46ffec3b8e3bb26bf207d0b
Carlos Jorge Cary Huanca <gary7412@hotmail.com>
2019-06-19 16:33:47 -0400

    we can add enemies with clicks

---
+++ Assets/Scripts/AddEnemies.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddEnemies : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform positionIndicator;
+
+    void Start(){
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            CreateEnemy(positionIndicator.position);
+        }
+    }
+
+    public void CreateEnemy(Vector3 pos) {
+        GameObject enemyCreated = Instantiate(enemy);
+        enemyCreated.transform.position = pos;
+        enemyCreated.transform.parent = transform.parent;
+    }
+}
---
+++ Assets/Scripts/Editor/AddEnemiesEditor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[CustomEditor(typeof(AddEnemies))]
+public class AddEnemiesEditor : Editor {
+
+    AddEnemies Target { get => (AddEnemies) target; }
+    
+    public override void OnInspectorGUI () {
+        if (GUILayout.Button("Crear enemigo!")) {
+            Target.CreateEnemy(Target.positionIndicator.position);
+        }
+
+        if (GUI.changed && !Application.isPlaying) {
+            EditorUtility.SetDirty(Target);
+            EditorSceneManager.MarkSceneDirty(Target.gameObject.scene);
+        }
+    }
+
+    void OnSceneGUI () {
+        DrawGizmos(Target);
+        RaycastHit hitInfo;
+        Ray worldRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+        
+        if (Physics.Raycast(worldRay, out hitInfo)) {
+            Debug.Log(hitInfo.point);
+            if (Event.current.type == EventType.MouseDown) {
+                // GUIUtility.hotControl = GUIUtility.GetControlID(FocusType.Passive);
+                // Event.current.Use();
+                Target.CreateEnemy(hitInfo.point);
+            }
+        }
+        
+        /*
+        Handles.color = new Color(1,0,0, 1);
+        
+         Handles.DrawSolidDisc(Target.transform.position, Vector3.up, 0.5f);
+        
+        if (Handles.Button(Target.transform.position, Quaternion.Euler(90, 0,0),
+                           0.5f, 0.5f, Handles.CircleHandleCap)) {
+            Debug.Log("!!!!??");
+        }
+        */
+
+        if (GUI.changed && ! Application.isPlaying) {
+            EditorUtility.SetDirty(Target);
+            EditorSceneManager.MarkSceneDirty(Target.gameObject.scene);
+        }
+    }
+
+    public static void DrawGizmos (AddEnemies customTarget) {
+        Handles.matrix = customTarget.transform.localToWorldMatrix;
+    }
+}
