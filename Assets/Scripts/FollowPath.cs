26e4e2118b66dede10632f69953f8d82d2f7ff6e
Carlos Jorge Cary Huanca <gary7412@hotmail.com>
2019-06-23 14:25:42 -0400

    Fixed draw lines and follow path

--- Assets/Scripts/FollowPath.cs
+++ Assets/Scripts/FollowPath.cs
@@ -5,31 +5,32 @@ using UnityEngine;
 public class FollowPath : MonoBehaviour
 {
     public float speed = 5;
-    public Transform path;
+    public Transform path = null;
     int child_number = 0;
     Vector3 target;
     int nChilds;
 
-    void Start()
-    {
-        nChilds = path.childCount - 1;
-        if (!IsSpeedPositive()){
-            child_number = nChilds;
+    void Start() {
+        if(path) {
+            nChilds = path.childCount - 1;
+            if (!IsSpeedPositive()){
+                child_number = nChilds;
+            }
+            findTarget();
         }
-        findTarget();
-        
     }
 
 
-    void Update()
-    {
-        float deltaDistance = Time.deltaTime * Mathf.Abs(speed);
-        transform.position =
-            Vector3.MoveTowards(transform.position,
-                                target,
-                                deltaDistance);
-        if (transform.position == target){
-            findTarget();
+    void Update() {
+        if(path) {
+            float deltaDistance = Time.deltaTime * Mathf.Abs(speed);
+            transform.position =
+                Vector3.MoveTowards(transform.position,
+                                    target,
+                                    deltaDistance);
+            if (transform.position == target){
+                findTarget();
+            }
         }
     }
 
@@ -65,5 +66,16 @@ public class FollowPath : MonoBehaviour
 
     bool IsSpeedPositive(){
         return speed > 0;
+    }
+
+    public List<Transform> GetAllChildsPath() {
+        List<Transform> listOfChilds = new List<Transform>();
+        if(path == null) {
+            return null;
+        }
+        foreach (Transform child in path) {
+              listOfChilds.Add(child);
+        }
+        return listOfChilds;
     }
 }
