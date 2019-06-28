37c7e878a706618286c0eb72ceeb9c548ff6b2a5
CarlosCary <44849210+CarlosCary@users.noreply.github.com>
2019-06-28 16:58:52 -0400

    Feature/add enemies with click (#31)
    * we can add enemies with clicks
    * we can add enemies with clicks and activate button
    * we can delete enemies with clicks
    * save scenes
    * cleaning code

---
+++ Assets/Scripts/AddEnemies.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddEnemies : MonoBehaviour
+{
+    public GameObject enemy;
+    static List<GameObject> listEnemies = new List<GameObject>();
+    
+    void Start(){
+    }
+
+    public GameObject CreateEnemy(Vector3 pos) {
+        GameObject enemyCreated = Instantiate(enemy);
+        enemyCreated.transform.position = pos;
+        enemyCreated.transform.parent = transform.parent;
+        InsertEnemy(enemyCreated);
+        return enemyCreated;
+    }
+
+    public List<GameObject> getEnemiesList() {
+        return listEnemies;
+    }
+
+    public void InsertEnemy(GameObject enemy) {
+        listEnemies.Add(enemy);
+    }
+
+    public void DestroyEnemy(GameObject enemy) {
+        for(int i = 0; i < listEnemies.Count; i++) {
+            if(listEnemies[i].transform.position == enemy.transform.position) {
+                if (Application.isEditor)
+                    Object.DestroyImmediate(listEnemies[i]);
+                listEnemies.RemoveAt(i);
+                break;
+            }
+        }
+    }
+
+    public void udpateListEnemies() {
+        for(int i = 0; i < listEnemies.Count; i++) {
+            if(listEnemies[i] == null) {
+                listEnemies.RemoveAt(i);
+            }
+        }
+    }
+}
