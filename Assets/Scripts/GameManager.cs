b2a40a3a88020bdd581e49be848cab15fe7c6a3e
Ruth <vengadoravg@gmail.com>
2019-07-01 13:14:16 -0400

    Feature/ui timer (#50)
    fix!
    
    * we can add enemies with clicks
    
    * we can add enemies with clicks and activate button
    
    * we can delete enemies with clicks
    
    * save scenes
    
    * fix bugs when delete enemies
    
    * cleaning code
    
    * Fixed draw lines and follow path
    
    * cleaning code
    
    * cleaning scene
    
    * enum para timed o temporized creado
    
    * metodo measureTIme in GameManager que controla el tiempo
    
    * elapsedTime y timecounting method ahora son staticas
    
    * cuando se carga un nivel el elapsed time se setea a 0

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,7 +10,11 @@ public class GameManager : MonoBehaviour
     public Object[] scenes;
     static int currentLevel;
     List<string> levels = new List<string>();
-
+    public TimeCountingMethod timeCountingMethod;
+    public float elapsedTime;
+    static bool gameEnd;
+    static int timeIncrease;
+    
     public static GameManager instance {
         get{ 
             if(_instance == null){
@@ -30,12 +34,34 @@ public class GameManager : MonoBehaviour
         }
         _instance = this;
         DontDestroyOnLoad(this.gameObject);
+        if (timeCountingMethod == TimeCountingMethod.Timed){
+            elapsedTime = 0;
+        }
+        gameEnd = false;
+        timeIncrease = 1;
+        
     }
 
     void Start() {
         RetrieveAllScenes();
         currentLevel = levels.IndexOf(SceneManager.GetActiveScene().name);
+        if (timeCountingMethod == TimeCountingMethod.Timed){
+            elapsedTime = 0;
+        }
+        
     }
+
+
+    void Update()
+    {
+        MeasureTime();
+        if (TimeIsUp()){
+            Defeat();
+        }
+    }
+
+
+
 
     void RetrieveAllScenes() {
         for (int i = 0; i < scenes.Length ; i++){
@@ -63,9 +89,41 @@ public class GameManager : MonoBehaviour
 
     public static void TriggerVictory() {
         UIManager.instance.ShowGameEndedOverlay(GameEndings.Victory); 
+        gameEnd = true;
+        timeIncrease = 0;
         ConffetiGunsManager.instance.activateConffetiGuns();
     }
-    
-    
-}
 
+    public static void  Defeat() {
+        UIManager.instance.ShowGameEndedOverlay(GameEndings.GameOver); 
+        gameEnd = true;
+        timeIncrease = 0;
+    }
+
+    void MeasureTime() {
+         switch(timeCountingMethod) {
+            case TimeCountingMethod.Temporized:
+                elapsedTime -= timeIncrease * Time.deltaTime;
+                break;
+            case TimeCountingMethod.Timed:
+                elapsedTime += timeIncrease * Time.deltaTime;
+                break;
+        }
+    }
+
+    bool TimeIsUp() {
+        return elapsedTime <= Mathf.Epsilon && !gameEnd;
+    }
+
+    public TimeCountingMethod getTimeCountingMethod() {
+        return _instance.timeCountingMethod;
+    }
+
+    public float getElapsedTime() {
+        return _instance.elapsedTime;
+    }
+
+    public bool isGameEnded() {
+        return gameEnd;
+    }
+}
