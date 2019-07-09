6ca0074cc0b454f7f36400f16fdf3a8844133fe8
Anstayker <christian.rojas.bl@gmail.com>
2019-07-09 17:28:57 -0400

    SandTrapManager to singleton

--- Assets/Scripts/SandTrapManager.cs
+++ Assets/Scripts/SandTrapManager.cs
@@ -6,7 +6,20 @@ using UnityEngine.SceneManagement;
 public class SandTrapManager : MonoBehaviour
 {
 
+    public static SandTrapManager instance = null;
     public List<GameObject> SandTrapList = new List<GameObject>();
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+
+        else if (instance != this) {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
 
     public void FindSandTrapsInTheScene() {
         foreach(GameObject GameObjectInTheScene in GameObject.FindObjectsOfType(typeof(GameObject)))
